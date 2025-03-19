93fd44ba3c10c0c7263756eec3a483e70ed4602d
kerosin <kerosin49@gmail.com>
2025-03-19 21:43:11 +0300

    First TZ

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello worl from Sasha!")
+
+import requests
 
 
-if __name__ == "__main__":
-    main()
+
+r = requests.get('https://dattebayo-api.onrender.com/characters')
+data = r.json()
+#print(data["characters"])
+c = data["characters"]
+print(c[0]["name"])
+
+for personaj in c:
+    print(f"Name - {personaj ["name"]}")
